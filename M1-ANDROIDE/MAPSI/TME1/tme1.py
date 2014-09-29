ae1fbb5d2bd3724d9a67fc4234d22db4aed88db0
hippunk <arthur.ramolet@hotmail.fr>
2014-09-29 02:26:27 +0200

    Avancement tme1 MAPSI

--- M1-ANDROIDE/MAPSI/TME1/tme1.py
+++ M1-ANDROIDE/MAPSI/TME1/tme1.py
@@ -20,8 +20,27 @@ liste = []
 #Suppression des arrondissements parasites
 for station in data:
     arrondissement = station['number']//1000
-    if arrondissement <= 0 or arrondissement > 20:
-        data.remove(station)
+    if arrondissement > 0 and arrondissement <= 20:
+        liste.append({'alt':station['alt'],'lng':station['position']['lng'],'lat':station['position']['lat'],'arr':station['number']//1000,'tot':station['bike_stands'],'disp':station['available_bikes']})
+
+#Calcul du nombre de stations par arrondissements
+nbArr = [0]*20
+for elem in liste:   
+    nbArr[elem['arr']-1]+=1
+
+#Division par le nombre de stations, Utilisation de pynum pour éviter boucle ?    
+probArr = []
+for elem in nbArr:   
+    probArr.append(elem/len(liste))
+ 
+#Affichages des probabilitées   
+#print(probArr)
+
+#debug somme proba, Utilisation de pynum pour éviter boucle ?
+#x = 0
+#for elem in probArr:   
+#    x+=elem
+#print(x)   
         
 #Debug traitement arrondissements parasites
 #for station in data:
